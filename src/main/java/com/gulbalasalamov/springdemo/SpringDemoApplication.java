a79be6e0818a9e399e1a54bf63f0497cae73925c
gulbalasalamov <g.salamov@gmail.com>
2021-09-09 16:21:51 +0300

    Student.java : Model class added SpringDemoApplication.java : returns an array of model class converted to JSON object autamitically by Spring Boot

--- src/main/java/com/gulbalasalamov/springdemo/SpringDemoApplication.java
+++ src/main/java/com/gulbalasalamov/springdemo/SpringDemoApplication.java
@@ -1,28 +1,46 @@
 package com.gulbalasalamov.springdemo;
 
+import com.gulbalasalamov.springdemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
 @RestController // to make this class serve as REST serve point
 public class SpringDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
 
 //	@GetMapping
 //	public String hello(){
 //		return "HelloWorld";
 //	}
 
-	@GetMapping //	//Annotate it to get something out of from server. This is rest point
-	public List<String> hello2(){
-		return List.of("Hello", "World","Gulbala","Salamov");
-	}
+    @GetMapping //	//Annotate it to get something out of from server. This is rest point
+    public List<Student> hello2() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Gulbala",
+                        "gulbala@gmail.com",
+                        LocalDate.of(1990, Month.NOVEMBER, 20),
+                        30
+                ),
+				new Student(
+						2L,
+						"Ahmet",
+						"ahmet@gmail.com",
+						LocalDate.of(1989,Month.SEPTEMBER,9),
+						31
+				)
+        );
+    }
 
 }
