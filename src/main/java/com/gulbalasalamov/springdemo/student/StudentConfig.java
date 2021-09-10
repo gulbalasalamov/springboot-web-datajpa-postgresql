7ab0ef2f8cbd1805fe260f1a1c4c9ebb8a0abbcc
gulbalasalamov <g.salamov@gmail.com>
2021-09-10 21:45:58 +0300

    StudentConfig.java : Configuration file having bean added and implemented

---
+++ src/main/java/com/gulbalasalamov/springdemo/student/StudentConfig.java
@@ -0,0 +1,42 @@
+package com.gulbalasalamov.springdemo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.NOVEMBER;
+
+@Configuration
+/**
+ * Configuration file having bean
+ */
+public class StudentConfig {
+
+    @Bean
+        //
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository) { //having access to repository. StudentRepository injected
+        return args -> {
+            Student gulbala = new Student(
+                    "Gulbala",
+                    "gulbala@gmail.com",
+                    LocalDate.of(1990, NOVEMBER, 20),
+                    30
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, NOVEMBER, 5),
+                    30
+            );
+            repository.saveAll(
+                    List.of(gulbala, alex)
+            );
+        };
+    }
+}
